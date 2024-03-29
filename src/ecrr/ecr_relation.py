52b790e6c3bb435ecc6f52003b689c741e3411e8
John W. <112655410+jozhw@users.noreply.github.com>
2024-03-29 09:49:59 -0500

    feat: entropy vs jpg/npz compressed ratio plot

--- src/ecrr/ecr_relation.py
+++ src/ecrr/ecr_relation.py
@@ -17,6 +17,9 @@ from src.calculations.calculate_results_by_file_type import (
     calculate_results_by_file_type,
 )
 from src.plotting.generate_ecrr_plot import generate_npz_ecrr_plot
+from src.plotting.generate_entropy_compressed_jpg_npz_plot import (
+    generate_entropy_compressed_jpg_npz_plot,
+)
 from src.plotting.generate_entropy_uncompressed_plot import (
     generate_entropy_uncompressed_plot,
 )
@@ -172,6 +175,12 @@ class ECrRelation:
 
     def gen_entropy_uncompressed_plot(self):
         generate_entropy_uncompressed_plot(
+            os.path.join(self.path_to_save_results_data, "results.csv"),
+            self.path_to_save_results_plot,
+        )
+
+    def gen_entropy_compressed_jpg_npz_plot(self):
+        generate_entropy_compressed_jpg_npz_plot(
             os.path.join(self.path_to_save_results_data, "results.csv"),
             self.path_to_save_results_plot,
         )
