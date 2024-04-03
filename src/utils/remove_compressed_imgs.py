52259ed580489d267d47486a43b68c9719c749d6
John Wu <j_zhw@proton.me>
2024-04-03 15:27:01 +0000

    refactor: os.remove -> os.unlink

--- src/utils/remove_compressed_imgs.py
+++ src/utils/remove_compressed_imgs.py
@@ -3,6 +3,6 @@ import os
 
 def remove_compressed_imgs(file_path: str):
     try:
-        os.remove(file_path)
+        os.unlink(file_path)
     except OSError as e:
         print(f"Error: {file_path} : {e.strerror}")
