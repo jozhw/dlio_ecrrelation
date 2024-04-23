3948d77ad1857bcf481a5d8f724cb6977524f7f2
John W. <112655410+jozhw@users.noreply.github.com>
2024-04-23 15:26:02 -0500

    update-fix: fix histogram x and y values and generate more plots

--- src/graph_intensity_distribution.py
+++ src/graph_intensity_distribution.py
@@ -2,19 +2,35 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from PIL import Image
 
 from calculations.calculate_entropy import count_occurrences
 
 
 def graph_intensity_distribution(
-    path_to_image: str, save_path="results/local/intensity_distributions/"
+    path_to_image: str,
+    path_to_data: str = "results/local/data/2024-04-23/synthetic_imgs_results.csv",
+    save_path="results/local/intensity_distributions/",
 ):
+
+    # get entropy and compression loss ratio
+    df = pd.read_csv(path_to_data)
 
     # get file name
     raw_fname = os.path.basename(path_to_image)
-
     fname = os.path.splitext(raw_fname)[0]
+    synthetic_name = "synthetic_{}.0".format(fname)
+
+    fname_row_match = df[df["file_name"] == synthetic_name]
+
+    if not fname_row_match.empty:
+        entropy = fname_row_match.iloc[0]["entropy"]
+        npz_compressed_synthetic_original_ratio = fname_row_match.iloc[0][
+            "npz_compressed_synthetic_original_ratio"
+        ]
+    else:
+        raise ValueError("No matching row found for {}".format(synthetic_name))
 
     # save path
     save_path = save_path + "{}.png".format(fname)
@@ -24,15 +40,22 @@ def graph_intensity_distribution(
 
     occurances = count_occurrences(image)
 
-    values = [occurances.get(key, 0) for key in range(256)]
-    plt.hist(values, bins=50, color="blue", alpha=0.7)
+    # Extract keys and values
+    keys = list(range(256))
+    values = [occurances.get(key, 0) for key in keys]
+
+    plt.figure(figsize=(12, 8))
+    plt.bar(keys, values, color="blue", alpha=0.7)
     plt.xlabel("Intensity Value")
     plt.ylabel("Frequency")
-    plt.title("Histogram of Intensity Values for {}".format(fname))
+    plt.title(
+        "Intensity Distribution for {} \nwith a NPZ Compressed Synthetic to Original Ratio of {} and Entropy of {}".format(
+            fname, npz_compressed_synthetic_original_ratio, entropy
+        )
+    )
     plt.grid(True)
 
     plt.savefig(save_path)
-
     plt.show()
 
 
@@ -43,6 +66,16 @@ if __name__ == "__main__":
         "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/823.jpg",
         "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/4217.jpg",
         "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/3578.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/10321.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/2666.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/5109.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/11981.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/10447.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/2100.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/11759.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/4571.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/7078.jpg",
+        "/Users/johnz.wu/Projects/Code/dlio_ecrrelation/assets/local/uncompressed_imgs/jpg/12250.jpg",
     ]
 
     for path in paths:
